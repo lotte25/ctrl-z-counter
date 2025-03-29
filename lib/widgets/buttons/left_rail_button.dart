788ef29324e31c3dac737acef52890b9389a9d8a
lotte25 <loldebi5@gmail.com>
2025-03-28 21:39:06 -0300

    Oops wrong color............

--- lib/widgets/buttons/left_rail_button.dart
+++ lib/widgets/buttons/left_rail_button.dart
@@ -24,7 +24,7 @@ class LeftRailButton extends StatelessWidget {
         child: IconButton(
           onPressed: onPressed, 
           icon: icon,
-          color: colorScheme.onPrimaryFixed
+          color: colorScheme.primaryContainer
         ),
       ),
     );
