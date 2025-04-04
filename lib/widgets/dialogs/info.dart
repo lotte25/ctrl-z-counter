2eb57a76894d53186e3df90ecf4e9c6a341cd256
lotte25 <loldebi5@gmail.com>
2025-04-03 21:28:06 -0300

    Forgot that one funny word

--- lib/widgets/dialogs/info.dart
+++ lib/widgets/dialogs/info.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
-showInfoDialog({
+void showInfoDialog({
   required BuildContext context
 }) {
   showDialog(
